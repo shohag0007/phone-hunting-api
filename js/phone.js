1d5ac8be591a68b248b48529ea8a06457680fccc
shohag0007 <merinedrive007@gmail.com>
2024-02-27 21:49:12 +0600

    phone hunting

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,35 @@
 const loadPhone = async() => {
 const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
 const data = await res.json();
-console.log(data);
+const phones = await data.data;
+//console.log(phones);
+displayPhones(phones);
 }
+
+const displayPhones = phones =>{
+ console.log(phones);
+ const phoneContainer = document.getElementById('phone-container');
+ phones.forEach(phone =>{
+ console.log(phone);
+ //2. create a div
+ const phoneCard = document.createElement('div');
+ phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
+ // 3. set inner Html
+ phoneCard.innerHTML =` <figure><img src="${phone.image}" alt="Shoes" /></figure>
+ <div class="card-body">
+   <h2 class="card-title">${phone.phone_name}</h2>
+   <p>If a dog chews shoes whose shoes does he choose?</p>
+   <div class="card-actions justify-end">
+     <button class="btn btn-primary">Buy Now</button>
+   </div>
+ </div>
+ `;
+ //appendChild
+ phoneContainer.appendChild(phoneCard);
+})
+ 
+ 
+    
+ }
 
 loadPhone();
